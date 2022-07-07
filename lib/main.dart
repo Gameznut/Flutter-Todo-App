dccb1d7fe2589fae3a607fdd7740868a77961e5b
Gameznut <emmanuelapabiekun@gmail.com>
2022-07-07 12:39:37 +0100

    Todo App

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Todo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Todo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +31,188 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final TextEditingController _todoController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String data = "";
+  final todos = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    FocusScopeNode currentFocus = FocusScope.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            SizedBox(
+              // height: 50,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: TextField(
+                        controller: _todoController,
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                        ),
+                        decoration: InputDecoration(
+                            hintText: 'What do you want to do',
+                            suffixIcon: InkWell(
+                                onTap: () {
+                                  _todoController.clear();
+                                },
+                                splashColor: Colors.red[300],
+                                child: const Icon(Icons.clear)),
+                            border: const OutlineInputBorder())),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Container(
+                      width: 100,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30)),
+                      child: MaterialButton(
+                        splashColor: Colors.blue,
+                        color: Colors.amber,
+                        onPressed: () => add(currentFocus),
+                        child: const Icon(Icons.add),
+                      ))
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 3),
+                        margin: index == 0
+                            ? const EdgeInsets.only(
+                                top: 10, left: 3, right: 3, bottom: 10)
+                            : const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 3),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5),
+                            boxShadow: [
+                              // BoxShadow(
+                              //     color: Colors.grey.shade400,
+                              //     offset: const Offset(-3, -3),
+                              //     blurRadius: 10.4,
+                              //     spreadRadius: 1.0),
+                              BoxShadow(
+                                  color: Colors.grey.shade500,
+                                  offset: const Offset(0, 3),
+                                  blurRadius: 10.4,
+                                  spreadRadius: 1.0)
+                            ]),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Text(
+                                todos[index].toString(),
+                                style: const TextStyle(fontSize: 25),
+                              ),
+                            ),
+                            // const SizedBox(
+                            //   width: 15,
+                            // ),
+                            Flexible(
+                                child: MaterialButton(
+                              splashColor: Colors.red,
+                              color: Colors.transparent,
+                              onPressed: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => Dialog(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(30.0)),
+                                          child: SizedBox(
+                                            height: 300,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Text(
+                                                    todos[index],
+                                                    style: const TextStyle(
+                                                      fontSize: 20.0,
+                                                    ),
+                                                ),
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    child: const Text("Close"))
+                                              ],
+                                            ),
+                                          ),
+                                        ));
+                              },
+                              child: const Icon(Icons.edit),
+                            )),
+                            Flexible(
+                                child: MaterialButton(
+                              splashColor: Colors.red,
+                              color: Colors.transparent,
+                              onPressed: () {
+                                setState(() {
+                                  todos.removeAt(index);
+                                });
+                              },
+                              child: const Icon(Icons.delete),
+                            )),
+                          ],
+                        ));
+                  }),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void add(currentFocus) {
+    setState(() {
+      data = _todoController.text;
+      _todoController.text.isEmpty ? null : todos.add(data);
+    });
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+    _todoController.text = "";
+  }
+
+// Future  openEditDialog() => showDialog(context: context, builder:         Dialog(
+//     shape: RoundedRectangleBorder(
+//     borderRadius:BorderRadius.circular(30.0)),
+// child: Container(
+// height: 300,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// FlutterLogo(size: 150,),
+// Text("This is a Custom Dialog",style:TextStyle(fontSize: 20),),
+// ElevatedButton(
+//
+// onPressed: (){
+// Navigator.of(context).pop();
+// }, child: Text("Close"))
+// ],
+// ))
 }
